dd1b8f924790678d6751b13416ddfe3ed931d497
hioz <hiozxy@gmail.com>
2020-08-04 20:51:37 +0800

    Updates

---
+++ precache-manifest.89ca003bf341e8e134919f99d0ce8dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf4826d17863cdc849be7f18c261839",
+    "url": "/punctuation-marks/index.html"
+  },
+  {
+    "revision": "ddeb396d6fec267650f2",
+    "url": "/punctuation-marks/static/css/main.2e7e9b96.chunk.css"
+  },
+  {
+    "revision": "d5a4621740e8a61ebe15",
+    "url": "/punctuation-marks/static/js/2.f19805b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/punctuation-marks/static/js/2.f19805b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddeb396d6fec267650f2",
+    "url": "/punctuation-marks/static/js/main.83645983.chunk.js"
+  },
+  {
+    "revision": "832fe9bfd9cdd086827b",
+    "url": "/punctuation-marks/static/js/runtime-main.f8d0d5e5.js"
+  }
+]);
